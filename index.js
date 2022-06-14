2cbd03f9419e1fd87716a4d2924ea424a6b92b8f
jorgeolivero <jolivero@tockall.com>
2022-06-14 14:29:19 -0500

    Ubicar en las coordenas usando three

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 window.onload = () => {
     let testEntitiesAdded = false;
-    alert('If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected.');
+    //alert('If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected.');
     const el = document.querySelector("[gps-new-camera]");
     el.addEventListener("gps-camera-update-position", e => {
         if(!testEntitiesAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+            console.log(e.detail.position.longitude)
+            console.log(e.detail.position.latitude)
             // Add four boxes to the north (red), south (yellow), west (blue)
             // and east (red) of the initial GPS position
             const properties = [{
