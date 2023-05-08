bcd997a75706a0f43a2534ccc712511cd174717b
jorgeolivero <jolivero@tockall.com>
2023-05-08 17:16:57 -0500

    Build classrooms

--- README.md
+++
@@ -1,3 +0,0 @@
-# ejemplo1_arjs
-Ejemplo para implementar arjs con un modelo 3d con una latitud y longitud
-Rama ejemplo6 con threejs
---
+++ assets/js/zyon-datepicker.js
@@ -0,0 +1,21 @@
+function zyonDatePicker(elem) {
+
+    if (elem) {
+
+        const datepicker = new Datepicker(elem, {
+
+            // allowOneSidedRange: true,
+            // autohide: true,
+            showOnFocus: false,
+            showOnClick: true,
+            todayHighligh: false,
+            language: 'es',
+            // title: 'Zyon',
+            // format: 'dd/mm/yyyy'
+
+        })
+
+
+    }
+
+}
